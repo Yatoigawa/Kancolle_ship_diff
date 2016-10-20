dcdda27741db4ddc603be16887d8ac31313c739f
Yatoigawa <flandrescarlet823@gmail.com>
2016-10-21 00:40:58 +0900

    リストビューの追加、ボタンの名前の変更

--- Kancolle_ship_diff/Kancolle_ship_diff/Form2.Designer.cs
+++ Kancolle_ship_diff/Kancolle_ship_diff/Form2.Designer.cs
@@ -28,82 +28,77 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.ListViewItem listViewItem1 = new System.Windows.Forms.ListViewItem(new string[] {
-            "",
-            "戦艦"}, -1);
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.buttonOK = new System.Windows.Forms.Button();
+            this.buttonCancel = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.sortableListView1 = new CSortableListViewLib.SortableListView();
-            this.CheckBoxes = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.ShipTypes = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.shipTypeListView = new CSortableListViewLib.SortableListView();
+            this.shipClassListView = new CSortableListViewLib.SortableListView();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // button1
+            // buttonOK
             // 
-            this.button1.DialogResult = System.Windows.Forms.DialogResult.OK;
-            this.button1.Location = new System.Drawing.Point(115, 306);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "OK";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.buttonOK.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.buttonOK.Location = new System.Drawing.Point(184, 277);
+            this.buttonOK.Name = "buttonOK";
+            this.buttonOK.Size = new System.Drawing.Size(75, 23);
+            this.buttonOK.TabIndex = 1;
+            this.buttonOK.Text = "OK";
+            this.buttonOK.UseVisualStyleBackColor = true;
+            this.buttonOK.Click += new System.EventHandler(this.buttonOK_Click);
             // 
-            // button2
+            // buttonCancel
             // 
-            this.button2.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.button2.Location = new System.Drawing.Point(196, 306);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "キャンセル";
-            this.button2.UseVisualStyleBackColor = true;
+            this.buttonCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.buttonCancel.Location = new System.Drawing.Point(265, 277);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(75, 23);
+            this.buttonCancel.TabIndex = 2;
+            this.buttonCancel.Text = "キャンセル";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
             // 
             // panel1
             // 
-            this.panel1.Controls.Add(this.sortableListView1);
-            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Controls.Add(this.shipTypeListView);
+            this.panel1.Controls.Add(this.shipClassListView);
+            this.panel1.Location = new System.Drawing.Point(13, 13);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(262, 288);
+            this.panel1.Size = new System.Drawing.Size(327, 258);
             this.panel1.TabIndex = 3;
             // 
-            // sortableListView1
+            // shipTypeListView
             // 
-            this.sortableListView1.CheckBoxes = true;
-            this.sortableListView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
-            this.CheckBoxes,
-            this.ShipTypes});
-            listViewItem1.StateImageIndex = 0;
-            this.sortableListView1.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
-            listViewItem1});
-            this.sortableListView1.Location = new System.Drawing.Point(0, 0);
-            this.sortableListView1.Name = "sortableListView1";
-            this.sortableListView1.Size = new System.Drawing.Size(126, 288);
-            this.sortableListView1.SortStyle = CSortableListViewLib.SortableListView.SortStyles.SortSelectedColumn;
-            this.sortableListView1.TabIndex = 0;
-            this.sortableListView1.UseCompatibleStateImageBehavior = false;
-            this.sortableListView1.View = System.Windows.Forms.View.Details;
+            this.shipTypeListView.Location = new System.Drawing.Point(161, 0);
+            this.shipTypeListView.Name = "shipTypeListView";
+            this.shipTypeListView.Size = new System.Drawing.Size(163, 258);
+            this.shipTypeListView.SortStyle = CSortableListViewLib.SortableListView.SortStyles.SortDefault;
+            this.shipTypeListView.TabIndex = 2;
+            this.shipTypeListView.UseCompatibleStateImageBehavior = false;
+            this.shipTypeListView.View = System.Windows.Forms.View.Details;
             // 
-            // CheckBoxes
+            // shipClassListView
             // 
-            this.CheckBoxes.Text = "○";
-            this.CheckBoxes.Width = 23;
-            // 
-            // ShipTypes
-            // 
-            this.ShipTypes.Text = "艦種";
-            this.ShipTypes.Width = 96;
+            this.shipClassListView.CheckBoxes = true;
+            this.shipClassListView.Location = new System.Drawing.Point(3, 0);
+            this.shipClassListView.Name = "shipClassListView";
+            this.shipClassListView.Size = new System.Drawing.Size(152, 258);
+            this.shipClassListView.SortStyle = CSortableListViewLib.SortableListView.SortStyles.SortDefault;
+            this.shipClassListView.TabIndex = 1;
+            this.shipClassListView.UseCompatibleStateImageBehavior = false;
+            this.shipClassListView.View = System.Windows.Forms.View.Details;
+            this.shipClassListView.SelectedIndexChanged += new System.EventHandler(this.shipTypeClassView_SelectedIndexChanged);
             // 
             // Form2
             // 
+            this.AcceptButton = this.buttonOK;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(287, 338);
+            this.CancelButton = this.buttonCancel;
+            this.ClientSize = new System.Drawing.Size(352, 308);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.buttonOK);
             this.Controls.Add(this.panel1);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
@@ -118,11 +113,10 @@
         }
 
         #endregion
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button buttonOK;
+        private System.Windows.Forms.Button buttonCancel;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.ColumnHeader CheckBoxes;
-        private System.Windows.Forms.ColumnHeader ShipTypes;
-        public CSortableListViewLib.SortableListView sortableListView1;
+        private CSortableListViewLib.SortableListView shipClassListView;
+        private CSortableListViewLib.SortableListView shipTypeListView;
     }
 }
