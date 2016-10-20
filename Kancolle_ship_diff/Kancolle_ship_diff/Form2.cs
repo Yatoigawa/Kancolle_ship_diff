a722149ba6cccd5e18eb61cb8719ba8894e072ce
Yatoigawa <flandrescarlet823@gmail.com>
2016-10-21 00:40:35 +0900

    初期表示の追加、ボタンが押された時の処理の追加

--- Kancolle_ship_diff/Kancolle_ship_diff/Form2.cs
+++ Kancolle_ship_diff/Kancolle_ship_diff/Form2.cs
@@ -20,22 +20,78 @@ namespace Kancolle_ship_diff
 
         private void Form2_Load(object sender, EventArgs e)
         {
+            // 艦種リスト用の文字列配列
+            string[] shipClassList =
+            {
+                "戦艦/航空戦艦",
+                "正規空母/装甲空母",
+                "軽空母",
+                "重巡洋艦/航空巡洋艦",
+                "重雷装巡洋艦",
+                "軽巡洋艦",
+                "駆逐艦",
+                "潜水艦/潜水空母",
+                "補給艦",
+                "水上機母艦",
+                "揚陸艦",
+                "工作艦",
+                "潜水母艦",
+                "練習巡洋艦"
+            };
+
+            // 列の追加
+            shipClassListView.Columns.Add("艦種", -2, HorizontalAlignment.Left);
+            shipTypeListView.Columns.Add("艦型", -2, HorizontalAlignment.Left);
+
+            // 項目の追加
+            for (int i = 0; i < shipClassList.Length; i++)
+            {
+                ListViewItem item_shipClassList = new ListViewItem();
+
+                item_shipClassList.Text = shipClassList[i];
+                shipClassListView.Items.Add(item_shipClassList);
+            }
+
 
         }
 
-        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        private void buttonOK_Click(object sender, EventArgs e)
         {
+            // 入力データ等の保存処理
+
+            // OKボタンが押された
+            this.DialogResult = System.Windows.Forms.DialogResult.OK;
+            // ShowDialog()による表示のため閉じる必要がある
 
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void buttonCancel_Click(object sender, EventArgs e)
         {
-            
+            // キャンセルボタンが押された
+            this.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            // ShowDialog()による表示のため閉じる必要がある
+            this.Close();
         }
 
-        private void checkedListBox2_SelectedIndexChanged(object sender, EventArgs e)
+        /// <summary>
+        /// 選択されている項目が変更された場合
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void shipTypeClassView_SelectedIndexChanged(object sender, EventArgs e)
         {
+            string idx;
+            if (shipClassListView.SelectedItems.Count > 0) {
+                // 選択されている項目の文字列を取得
+                idx = shipClassListView.SelectedItems[0].Text;
 
+                // 現在表示されているリストビューの項目の総数を取得
+                int shipTypeList_max  = shipTypeListView.Items.Count;
+                for (int i = 0; i < shipTypeList_max; i++)
+                {
+
+                }
+            }
         }
     }
 }
